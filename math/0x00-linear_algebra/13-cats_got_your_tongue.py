a14020512ba2bc613577c141cd5e6b27e7025548
jofurdz <jofurdz@gmail.com>
2022-02-04 14:35:11 -0600

    documentation for task 13

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""function to concatenate matrices along specific axis"""
 
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
+    """concatenates 2 matrices along specific axis"""
     new_list = []
     a = np.array(mat1)
     b = np.array(mat2)
