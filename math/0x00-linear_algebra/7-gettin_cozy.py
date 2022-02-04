14e0b72fcfb17a2a6aff1850da7d945fb26a33d2
jofurdz <jofurdz@gmail.com>
2022-02-04 13:07:28 -0600

    added README

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -2,3 +2,14 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenates two  matrices"""
+    if axis == 0:
+
+    else:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            newMatrix = []
+            for x in  range(len(mat1)):
+                newMatrix.append(mat1[x] + mat2[x])
+            return newMatrix
