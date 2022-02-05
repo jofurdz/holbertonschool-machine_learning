77dec2a563d662fe1103d94a67003ae35f471349
jofurdz <jofurdz@gmail.com>
2022-02-05 14:53:14 -0600

    fixed task 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,10 +4,21 @@
 
 def add_matrices2D(mat1, mat2):
     """adds two matrices elemt-wise"""
-    newMatrix = []
-    for a, b in zip(mat1, mat2):
-        current_list = []
-        for x, y in zip(a, b):
-            current_list.append(x + y)
-        newMatrix.append(current_list)
-    return newMatrix
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    else:
+        newMatrix = []
+        for a, b in zip(mat1, mat2):
+            current_list = []
+            for x, y in zip(a, b):
+                current_list.append(x + y)
+                newMatrix.append(current_list)
+            return newMatrix
+
+
+def matrix_shape(matrix):
+    """"returns shape of matrix"""
+    try:
+        return [len(matrix)] + matrix_shape(matrix[0])
+    except Exception:
+        return []
