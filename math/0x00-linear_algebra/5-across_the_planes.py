20963aa5e1381305262903741206614663c2097a
jofurdz <jofurdz@gmail.com>
2022-02-05 15:00:24 -0600

    task 5 update

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -12,8 +12,8 @@ def add_matrices2D(mat1, mat2):
             current_list = []
             for x, y in zip(a, b):
                 current_list.append(x + y)
-                newMatrix.append(current_list)
-            return newMatrix
+            newMatrix.append(current_list)
+        return newMatrix
 
 
 def matrix_shape(matrix):
