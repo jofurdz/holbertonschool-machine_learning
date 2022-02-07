80ca31d3b293a8cb805396076518b2ae47790f0d
jofurdz <jofurdz@gmail.com>
2022-02-07 07:51:57 -0600

    finished task 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,12 +5,20 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two  matrices"""
     if axis == 0:
-
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        else:
+            result = []
+            for row in mat1:
+                result.append(row.copy())
+            for j in range(len(mat2)):
+                result.append(mat2[j].copy())
+            return result
     else:
         if len(mat1) != len(mat2):
             return None
         else:
             newMatrix = []
-            for x in  range(len(mat1)):
+            for x in range(len(mat1)):
                 newMatrix.append(mat1[x] + mat2[x])
             return newMatrix
