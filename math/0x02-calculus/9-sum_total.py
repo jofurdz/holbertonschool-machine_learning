a18f465710d2fa4f06584c67b629a3e0235dc683
jofurdz <jofurdz@gmail.com>
2022-02-14 13:15:44 -0600

    adding documentation/ updating 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,9 +2,10 @@
 
 
 def summation_i_squared(n):
-    if n == 1:
-        return 1
-    elif n <= 0:
+    """returns summation of i**2 to n"""
+    if n < 1:
         return None
+    elif n == 1:
+        return 1
     else:
         return (n**2 + summation_i_squared(n-1))
