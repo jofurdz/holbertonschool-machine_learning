160bbe2bef6f4044199046835c8f68a152c77817
jofurdz <jofurdz@gmail.com>
2022-02-19 10:15:31 -0600

    task 1 and 2

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -23,3 +23,26 @@ class Poisson():
                 for x in data:
                     total = x + total
                 self.lambtha = float(total / length)
+
+    def pmf(self, k):
+        e = 2.7182818285
+        pmf_numerator = (e ** (self.lambtha * -1) * (self.lambtha ** k))
+        pmf_denominator = 1
+        if type(k) is not int:
+            k = int(key)
+        for x in range(1, k + 1):
+            pmf_denominator *= x
+        return pmf_numerator / pmf_denominator
+
+    def cdf(self, k):
+        e = 2.7182818285
+        cdf_store = []
+        if type(k) is not int:
+            k = int(key)
+        for i in range(k + 1):
+            cdf_numerator = (e ** (self.lambtha * -1) * (self.lambtha ** i))
+            cdf_denominator = 1
+            for x in range(1, i + 1):
+                cdf_denominator *= x
+            cdf_store.append(cdf_numerator / cdf_denominator)
+        return sum(cdf_store)
