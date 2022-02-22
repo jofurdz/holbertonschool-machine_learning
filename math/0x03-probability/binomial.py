3d8a86d3a4cd66876179756e77230ee55206911a
jofurdz <jofurdz@gmail.com>
2022-02-21 23:10:52 -0600

    init for binomial

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,16 +9,25 @@ class Binomial():
     """initializes class Binomial"""
     def __init__(self, data=None, n=1, p=0.5):
         if data is None:
-            if n is <= 0:
+            if n <= 0:
                 raise ValueError("n must be a positive value")
-            elif p """ask mango""":
+            elif p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
             else:
-                """??????"""
+                self.n = n
+                self.p = p
         else:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             elif type(data) is not list:
                 raise TypeError("data must be a list")
             else:
-                
+                mean = float(sum(data) / len(data))
+                dev = [(i - mean) ** 2 for i in data]
+                vari = sum(dev) / len(data)
+                q = vari / mean
+                p = 1 - q
+                n = round(mean / p)
+                p = float(mean / n)
+                self.n = n
+                self.p = p
