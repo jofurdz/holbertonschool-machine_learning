96a7f2339e6a30d5ce2ae10c251827d3e06d967b
jofurdz <jofurdz@gmail.com>
2022-03-07 15:20:15 -0600

    task 16 maybe??

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,26 @@
+#!/usr/bin/ennv python3
+"""function for deep nueral network"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """initializes class"""
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        prev = nx
+        for x in range(self.L):
+            if type(layers[x]) is not int or layers[x] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            w = "W{}".format(x + 1)
+            b = "b{}".format(x + 1)
+            self.weights[b] = np.zeros((layers[x], 1))
+            self.weights[w] = np.random.randn(layers[x], prev) * np.sqrt(2 / prev)
+            prev = layers[x]
