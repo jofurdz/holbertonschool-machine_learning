93919547d4afca47adf58e689b786db7b5dffa9b
jofurdz <jofurdz@gmail.com>
2022-03-12 11:40:25 -0600

    task 1 update

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -6,4 +6,5 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """creates layers for nueral network"""
     w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    return tf.layers.Dense(n, activation, kernel_initializer=w, name='layer')
+    l = tf.layers.Dense(n, activation, kernel_initializer=w, name='layer')
+    return l(prev)
