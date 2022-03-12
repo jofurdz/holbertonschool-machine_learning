782f382571da3d0bc0a368a8a6f7cd1c4e110d3c
jofurdz <jofurdz@gmail.com>
2022-03-12 11:28:07 -0600

    pycode

--- supervised_learning/0x00-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x00-tensorflow/0-create_placeholders.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""returns two placeholders for the nueral network"""
-import tensorflow as tf:
+"""function to create placeholders for the nueral network"""
+import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
+    """creates placeholders"""
     x = tf.placeholder("float", shape=(None, nx), name='x')
     y = tf.placeholder("float", shape=(None, classes), name='y')
     return (x, y)
