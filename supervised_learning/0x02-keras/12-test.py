1280acdf0cfafe7f580dbfad7229e9e30d81a48b
jofurdz <jofurdz@gmail.com>
2022-03-31 18:51:25 -0500

    task 12

--- supervised_learning/0x02-keras/12-test.py
+++ supervised_learning/0x02-keras/12-test.py
@@ -5,4 +5,4 @@ import tensorflow.keras as K
 
 def test_model(network, data, labels, verbose=True):
     """tests neural network"""
-    return network.test(x=data, y=labels, verbose=verbose)
+    return network.evaluate(x=data, y=labels, verbose=verbose)
