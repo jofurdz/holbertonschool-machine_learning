67a9c0243771ab4e3b0dbe8d95ee0b93334b5e98
jofurdz <jofurdz@gmail.com>
2022-04-20 10:30:42 -0500

    task 2 update

--- supervised_learning/0x01-deep_cnns/2-identity_block.py
+++ supervised_learning/0x01-deep_cnns/2-identity_block.py
@@ -13,11 +13,11 @@ def identity_block(A_prev, filters):
     b1 = K.layers.BatchNormalization(axis=3)(c1)
     a1 = K.layers.Activation('relu')(b1)
     c2 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                         kernel_initializer=init)(b1)
+                         kernel_initializer=init)(a1)
     b2 = K.layers.BatchNormalization(axis=3)(c2)
     a2 = K.layers.Activation('relu')(b2)
     c3 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
-                         kernel_initializer=init)(b2)
+                         kernel_initializer=init)(a2)
     b3 = K.layers.BatchNormalization(axis=3)(c3)
     add = K.layers.Add()([b3, A_prev])
     poopla = K.layers.Activation('relu')(add)
