472db66fd284539e48524c59274d159140d34d0a
jofurdz <jofurdz@gmail.com>
2022-04-20 10:45:58 -0500

    update to task 3

--- supervised_learning/0x01-deep_cnns/3-projection_block.py
+++ supervised_learning/0x01-deep_cnns/3-projection_block.py
@@ -9,18 +9,18 @@ def projection_block(A_prev, filters, s=2):
     init = K.initializers.he_normal(seed=None)
 
     c1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), padding='same',
-                         kernel_initializer=init)(A_prev)
+                         kernel_initializer=init, strides=s)(A_prev)
     b1 = K.layers.BatchNormalization(axis=3)(c1)
     a1 = K.layers.Activation('relu')(b1)
     c2 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                         kernel_initializer=init)(a1)
+                         kernel_initializer=init, strides=1)(a1)
     b2 = K.layers.BatchNormalization(axis=3)(c2)
     a2 = K.layers.Activation('relu')(b2)
     c3 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
-                         kernel_initializer=init)(a2)
+                         kernel_initializer=init, strides=1)(a2)
     b3 = K.layers.BatchNormalization(axis=3)(c3)
     c4 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
-                         kernel_initializer=init)(A_prev)
+                         kernel_initializer=init, strides=s)(A_prev)
     b4 = K.layers.BatchNormalization(axis=3)(c4)
     add = K.layers.Add()([b3, b4])
     return K.layers.Activation('relu')(add)
