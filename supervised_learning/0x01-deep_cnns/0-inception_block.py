4a6625c1c4aeec739c2321d2efb0bc8f7b6eeab3
jofurdz <jofurdz@gmail.com>
2022-04-20 10:06:15 -0500

    task 0 update

--- supervised_learning/0x01-deep_cnns/0-inception_block.py
+++ supervised_learning/0x01-deep_cnns/0-inception_block.py
@@ -15,19 +15,19 @@ def inception_block(A_prev, filters):
     conv2 = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1), padding='same',
                             activation='relu',
                             kernel_initializer=init)(A_prev)
+    conv4 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
+                            activation='relu',
+                            kernel_initializer=init)(conv2)
     conv3 = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1), padding='same',
                             activation='relu',
                             kernel_initializer=init)(A_prev)
     # print("{}".format(A_prev.shape))
-    pl = K.layers.MaxPooling2D(pool_size=(3, 3), strides=1,
-                               padding='same')(A_prev)
     # second group
-    conv4 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                            activation='relu',
-                            kernel_initializer=init)(conv2)
     conv5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5), padding='same',
                             activation='relu',
                             kernel_initializer=init)(conv4)
+    pl = K.layers.MaxPooling2D(pool_size=(3, 3), strides=1,
+                               padding='same')(A_prev)
     conv6 = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1), padding='same',
                             activation='relu',
                             kernel_initializer=init)(pl)
