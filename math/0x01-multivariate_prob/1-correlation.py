c4a72005eceadae4c8e6c51c1601b87e232f677b
jofurdz <jofurdz@gmail.com>
2022-06-09 16:42:25 -0500

    task 1

--- math/0x01-multivariate_prob/1-correlation.py
+++ math/0x01-multivariate_prob/1-correlation.py
@@ -5,6 +5,12 @@ import numpy as np
 
 def correlation(C):
     """calculates correlation matrix"""
+    if type(C) != np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+    if C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
     variances = np.diag(C)
     stddevs = np.sqrt(variances)
     # divide horizontally, then vertically
