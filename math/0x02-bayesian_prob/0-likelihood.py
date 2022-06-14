a522f8fde33cc5d9b72b30139805e81b37a6c432
jofurdz <jofurdz@gmail.com>
2022-06-14 14:33:42 -0500

    update to task 0

--- math/0x02-bayesian_prob/0-likelihood.py
+++ math/0x02-bayesian_prob/0-likelihood.py
@@ -13,13 +13,16 @@ def ncr(n, r):
 
 def likelihood(x, n, P):
     """calculates the probability using bayesian distribution"""
-    if type(n) is not int or n < 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if type(x) is not int or x <= 0:
+    if type(x) is not int or x < 0:
         raise ValueError("x must be an integer\
          that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray:
+    if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if True in  ((P < 0) + (P > 1)):
+        err = "All values in P must be in the range [0, 1]"
+        raise ValueError(err)
     return ncr(n, x) * (P ** x) * ((1 - P) ** (n - x))
