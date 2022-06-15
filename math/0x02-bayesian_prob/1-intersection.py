640362c461809fc39086c5b57bed4232ef404e8b
jofurdz <jofurdz@gmail.com>
2022-06-14 19:03:36 -0500

    task 0 and 1 update

--- math/0x02-bayesian_prob/1-intersection.py
+++ math/0x02-bayesian_prob/1-intersection.py
@@ -30,15 +30,22 @@ def likelihood(x, n, P):
 
 def intersection(x, n, P, Pr):
     """calculates intersectionality"""
-    summ = np.sum(Pr)
-    if type(Pr) is not np.ndarray or np.shape(Pr) != P.shape:
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.count_nonzero((P < 0) | (P > 1)) > 0:
-        raise ValueError("All values in {} must be in the range\
-            [0, 1]".format(P[((P < 0) | (P > 1)).argmax()]))
+        raise ValueError("All values in P must be in the range [0, 1]")
     if np.count_nonzero((Pr < 0) | (Pr > 1)) > 0:
-        raise ValueError("All values in {} must be in the range\
-            [0, 1]".format(Pr[((Pr < 0) | (Pr > 1)).argmax()]))
-    if summ != 1:
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if False in np.isclose([Pr.sum()], [1]):
         raise ValueError("Pr must sum to 1")
-    return (likelihood(x, n, P) * Pr)
+    return(Pr * likelihood(x, n, P))
