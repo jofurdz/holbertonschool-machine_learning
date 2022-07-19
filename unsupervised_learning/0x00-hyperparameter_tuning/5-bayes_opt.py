a29ed35955643b1c5d86ea445a7f3b485a70389b
jofurdz <jofurdz@gmail.com>
2022-07-18 22:16:26 -0500

    task 5 empty function

---
+++ unsupervised_learning/0x00-hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""module containing class BayesianOptimization"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization():
+    """creating class"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """initializing class"""
+        min, max = bounds
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.xsi = xsi
+        self.minimize = minimize
+        self.X_s = np.linspace(min, max).reshape(-1, 1)
+
+    def acquisition(self):
+        """expected improvement acquisition function"""
+
+    def optimize(self, iterations=100):
+        """optimizes black-box function"""
