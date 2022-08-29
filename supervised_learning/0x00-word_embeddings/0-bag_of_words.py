7af8d0f1da21b112bb75181197c0a7addc1fe7a1
jofurdz <jofurdz@gmail.com>
2022-08-29 11:30:54 -0500

    updated get_feature_names_out to an older version

--- supervised_learning/0x00-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x00-word_embeddings/0-bag_of_words.py
@@ -8,6 +8,6 @@ def bag_of_words(sentences, vocab=None):
     vectorizer = CountVectorizer(vocabulary=vocab)
     newArray = vectorizer.fit_transform(sentences)
     embeddings = newArray.toarray()
-    features = vectorizer.get_feature_names_out()
+    features = vectorizer.get_feature_names()
 
     return embeddings, features
